50c9904ce5ca6e3e7b74b9c2d59fe50f5bf748ee
Guilherme de Castro Millares <guilherme.millares@idealtrends.local>
2026-08-11 12:56:36 -0300

    Add Traducao

--- citacoes.py
+++ citacoes.py
@@ -2,7 +2,12 @@ import requests
 
 resposta = requests.get("https://zenquotes.io/api/random")
 dados = resposta.json()
-frases = dados[0]["q"]
+frase = dados[0]["q"]
 autor = dados[0]["a"]
 
-print(f'"{frases}" — {autor}')
+resposta_traducao= requests.get("https://api.mymemory.translated.net/get", params={"q": frase, "langpair": "en|pt"})
+dados_traducao = resposta_traducao.json()
+frase_traduzida = dados_traducao["responseData"]["translatedText"]
+
+print(f'"{frase_traduzida}" — {autor}')
+# print(f'"{frases}" — {autor}')
